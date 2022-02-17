5c231ed78953278df2f01e1eed0eb3b5ec44835b
irakli9909 <razora.irakli@gmail.com>
2022-02-17 18:43:56 +0400

    #1.3.4 SlideKarel

---
+++ 1.3.4:SlideKarel.js
@@ -0,0 +1,17 @@
+putBall();
+move();
+turnRight();
+
+function turnRight(){
+    turnLeft();
+    turnLeft();
+    turnLeft();
+}
+move();
+putBall();
+turnLeft();
+move();
+turnRight();
+move();
+putBall();
+turnLeft();
