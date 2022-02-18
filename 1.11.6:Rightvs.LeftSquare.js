256b1c5996a3a2095beb08576b56ff01e34ba9a6
irakli9909 <razora.irakli@gmail.com>
2022-02-18 17:55:31 +0400

    #1.11.6 right vs left square

---
+++ 1.11.6:Rightvs.LeftSquare.js
@@ -0,0 +1,21 @@
+function start(){
+    if(facingEast()){
+        makeLeftSquare();
+    }else{
+        makeRightSquare();
+    }
+}
+function makeLeftSquare(){
+    for (var i = 0; i < 4; i++){
+        putBall();
+        move();
+        turnLeft();
+    }
+}
+function makeRightSquare(){
+    for (var i = 0; i < 4; i++){
+        putBall();
+        move();
+        turnRight();
+    }
+}
