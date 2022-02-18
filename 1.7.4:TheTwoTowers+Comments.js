2b2eb40356181ae5d54663d94337b0dd490acb6e
irakli9909 <razora.irakli@gmail.com>
2022-02-18 17:32:38 +0400

    #1.7.4 The Two Towers + Comments

---
+++ 1.7.4:TheTwoTowers+Comments.js
@@ -0,0 +1,46 @@
+/*This program is how to build two towers.
+*It is very simple
+*/
+function start(){
+	move();
+	buildCoins();
+	turnRight();
+	move();
+	turnRight();
+	move();
+	move();
+	turnLeft();
+	move();
+	buildCoinsFinish();
+}
+
+/*This function is build towers
+*/
+function buildCoins(){
+    turnLeft();
+    putBall();
+    move();
+    putBall();
+    move();
+    putBall();
+} 
+
+//This is turn right function
+function turnRight(){
+    turnLeft();
+    turnLeft();
+    turnLeft();
+}
+
+/*this is build and finish towers
+*/
+function buildCoinsFinish(){
+    turnLeft();
+    putBall();
+    move();
+    putBall();
+    move();
+    putBall();
+    move();
+    turnRight();
+}
