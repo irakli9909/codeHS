45a8591494775661988f9f7124933887f539ae76
irakli9909 <razora.irakli@gmail.com>
2022-02-18 17:16:01 +0400

    #1.4.4 pancakes

---
+++ 1.4.4:Pancakes.js
@@ -0,0 +1,18 @@
+move();
+makePancakes();
+function makePancakes(){
+    putBall();
+    putBall();
+    putBall();
+}
+
+moveTwice();
+function moveTwice(){
+    move();
+    move();
+}
+
+makePancakes();
+moveTwice();
+makePancakes();
+move();
