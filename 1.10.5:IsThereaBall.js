9f89ec339da968fe3a7f02296d6e5f17bce68356
irakli9909 <razora.irakli@gmail.com>
2022-02-18 17:50:15 +0400

    #1.10.5 is there a ball?

---
+++ 1.10.5:IsThereaBall.js
@@ -0,0 +1,14 @@
+// Karel should put a ball on the first spot
+// if there isn't one already there and then move.
+function start() {
+    hasBallTrue();
+}
+
+function hasBallTrue(){
+    if(ballsPresent()){
+        move();   
+    }else{
+        putBall();
+        move();
+    }
+}
