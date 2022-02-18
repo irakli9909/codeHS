809df29fb88af90bb453d43c7a2bd19fbbf1e429
irakli9909 <razora.irakli@gmail.com>
2022-02-18 17:19:08 +0400

    #1.4.5 mario karel

---
+++ 1.4.5:MarioKarel.js
@@ -0,0 +1,39 @@
+move();
+collectCoins();
+function collectCoins(){
+    turnLeft();
+    move();
+    move();
+    move();
+}
+
+takeCoins();
+function takeCoins(){
+    takeBall();
+    takeBall();
+}
+back();
+function back(){
+    turnLeft();
+    turnLeft();
+    move();
+    move();
+    move();
+    turnLeft();
+}
+move();
+move();
+    
+collectCoins();
+takeCoins();
+back();
+move();
+move();
+collectCoins();
+takeCoins();
+back();
+move();
+move();
+collectCoins();
+takeCoins();
+back();
