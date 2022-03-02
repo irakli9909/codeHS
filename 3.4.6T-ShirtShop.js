c0a1d1d2350dbeb3e1f786d985938cd725c5bce8
irakli9909 <razora.irakli@gmail.com>
2022-03-02 10:53:15 +0400

    #3.4.6 T- shirt Shop

---
+++ 3.4.6T-ShirtShop.js
@@ -0,0 +1,8 @@
+//Declare a constant here to represent the cost of a tshirt
+
+function start(){
+	/* Write your code here, and get rid of this comment */
+	var COST_OF_SHIRT = 20;
+    var amount = readInt("Amount OF Shirts:");
+    println("Shirts cost is " + amount * COST_OF_SHIRT + "$");
+}
